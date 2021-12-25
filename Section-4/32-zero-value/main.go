9f51083a37666683d024d20fa0d0f6b25e2a9c0d
u.dukenbay <urziya.dukenbay@gmail.com>
2021-12-25 22:32:03 +0600

    You've done great

---
+++ Section-4/32-zero-value/main.go
@@ -0,0 +1,21 @@
+package main
+
+import ("fmt")
+
+var y string
+var z int
+
+func main() {
+	// DECLARE a variable to be of a certain TYPE
+	// and then ASSIGN a value of that type to the variable
+	fmt.Println("printing the value of y", y, "ending")
+	fmt.Printf("%T\n", y)
+
+	y = "Bond, James Bond"
+
+	fmt.Println("printing the value of y", y, "ending")
+	fmt.Printf("%T\n", y)
+
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+}
