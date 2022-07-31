7f7bf1a77598b079920cedb0acdf12254ef2af7e
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-07-31 17:40:31 +0600

    func even(f func(xi ...int) int, vi ...int) --> func(xi ...int) int callback function as an argument which passed into even function

--- Section-14/118-callbacks/main.go
+++ Section-14/118-callbacks/main.go
@@ -10,7 +10,13 @@ import "fmt"
 func main() {
 	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	s := sum(ii...)
-	fmt.Println(s)
+	fmt.Println("All numbers:", s)
+
+	s2 := even(sum, ii...)
+	fmt.Println("Even numbers:", s2)
+
+	s3 := odd(sum, ii...)
+	fmt.Println("Odd numbers:", s3)
 }
 
 func sum(xi ...int) int {
@@ -21,4 +27,25 @@ func sum(xi ...int) int {
 	}
 
 	return total
+}
+
+//func(xi ...int) int callback function as an argument which passed into even function
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}
+
+func odd(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 != 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
 }
